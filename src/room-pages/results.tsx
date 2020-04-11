2f4ced69a184ad243fcc467141773ae3f6a4f3b8
Yurick <yurick.hauschild@gmail.com>
2020-04-11 09:43:00 +0200

    ✨ Show all votes

--- src/room-pages/results.tsx
+++ src/room-pages/results.tsx
@@ -23,6 +23,16 @@ const BackNumber = ({ children }: { children: string | number }) => (
   </div>
 )
 
+const byEstimative = (
+  [, est1]: [string, string],
+  [, est2]: [string, string],
+) => {
+  if (est1 === 'Too much') return -1
+  if (est2 === 'Too much') return 1
+
+  return parseInt(est1) - parseInt(est2)
+}
+
 export const Results: React.FunctionComponent<ResultsProps> = ({
   roomId,
   activeTicket,
@@ -43,44 +53,65 @@ export const Results: React.FunctionComponent<ResultsProps> = ({
       <p className="subtitle">{activeTicket}</p>
 
       {votes.length > 0 && (
-        <div className="columns">
-          <div className="column">
-            <article className="box" style={{ position: 'relative' }}>
-              <h5 className="subtitle is-5">Highest</h5>
+        <div className="column">
+          <div className="columns">
+            <div className="column">
+              <article className="box" style={{ position: 'relative' }}>
+                <h5 className="subtitle is-5">Highest</h5>
 
-              <div className="content">
-                <ul>
-                  {Object.entries(results)
-                    .filter(([, value]) => value === highest.toString())
-                    .sort()
-                    .map(([username]) => (
-                      <li key={username}>{username}</li>
-                    ))}
-                </ul>
-              </div>
+                <div className="content">
+                  <ul>
+                    {Object.entries(results)
+                      .filter(([, value]) => value === highest.toString())
+                      .sort()
+                      .map(([username]) => (
+                        <li key={username}>{username}</li>
+                      ))}
+                  </ul>
+                </div>
 
-              <BackNumber>{highest}</BackNumber>
-            </article>
+                <BackNumber>{highest}</BackNumber>
+              </article>
+            </div>
+
+            <div className="column">
+              <article className="box" style={{ position: 'relative' }}>
+                <h5 className="subtitle is-5">Lowest</h5>
+
+                <div className="content">
+                  <ul>
+                    {Object.entries(results)
+                      .filter(([, value]) => value === lowest.toString())
+                      .sort()
+                      .map(([username]) => (
+                        <li key={username}>{username}</li>
+                      ))}
+                  </ul>
+                </div>
+
+                <BackNumber>{lowest}</BackNumber>
+              </article>
+            </div>
           </div>
 
-          <div className="column">
-            <article className="box" style={{ position: 'relative' }}>
-              <h5 className="subtitle is-5">Lowest</h5>
-
-              <div className="content">
-                <ul>
-                  {Object.entries(results)
-                    .filter(([, value]) => value === lowest.toString())
-                    .sort()
-                    .map(([username]) => (
-                      <li key={username}>{username}</li>
-                    ))}
-                </ul>
-              </div>
-
-              <BackNumber>{lowest}</BackNumber>
-            </article>
-          </div>
+          <table className="table is-striped is-narrow is-hoverable is-fullwidth">
+            <thead>
+              <tr>
+                <th>Teammember</th>
+                <th>Estimative</th>
+              </tr>
+            </thead>
+            <tbody>
+              {Object.entries(results)
+                .sort(byEstimative)
+                .map(([username, estimative]) => (
+                  <tr key={username}>
+                    <td>{username}</td>
+                    <td>{estimative}</td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
         </div>
       )}
     </PageLayout>
