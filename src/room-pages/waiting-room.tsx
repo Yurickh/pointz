3d825b50df9391400f4184d9497359dbbd61e2cb
Yurick <yurick.hauschild@gmail.com>
2020-04-11 17:02:08 +0200

    ✨ Show the link copier

--- src/room-pages/waiting-room.tsx
+++ src/room-pages/waiting-room.tsx
@@ -45,6 +45,8 @@ export const WaitingRoom = ({
 }: RouteComponentProps<{ roomId: string }>) => {
   const [, setActiveTicket] = useActiveTicket(roomId)
   const [isCreating, setIsCreating] = React.useState(false)
+  const link = React.useRef(null)
+  const [copiedVisible, setCopiedVisible] = React.useState(false)
 
   if (isCreating) {
     return (
@@ -70,12 +72,53 @@ export const WaitingRoom = ({
         Here you can either wait for your teammembers to create a request for
         votes, or you can do it yourself.
       </p>
+      <p className="subtitle is-family-secondary">
+        Feel free to invite them here by sharing the link:
+      </p>
+
+      <div className="field has-addons">
+        <div className="control is-expanded">
+          <input
+            className="input is-family-primary"
+            readOnly
+            value={`https://pointz.yurick.dev/room/${roomId}`}
+            ref={link}
+          />
+        </div>
+        <div className="control">
+          <span
+            className="is-family-secondary"
+            style={{
+              opacity: copiedVisible ? 1 : 0,
+              transition: 'opacity ease .3s',
+              position: 'absolute',
+              top: '-2em',
+              right: '1ch',
+            }}
+          >
+            Copied!
+          </span>
+          <button
+            className="button is-info is-family-secondary"
+            onClick={() => {
+              if (link.current) {
+                link.current.select()
+                document.execCommand('copy')
+                setCopiedVisible(true)
+                setTimeout(() => setCopiedVisible(false), 1000)
+              }
+            }}
+          >
+            copy
+          </button>
+        </div>
+      </div>
 
       <button
         className="button is-primary is-family-primary"
         onClick={() => setIsCreating(true)}
       >
-        Create a request for votes
+        Create a ticket for voting
       </button>
     </PageLayout>
   )
