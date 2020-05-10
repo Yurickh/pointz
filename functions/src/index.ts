21c69d520352127eadf445e82f50138fc139b240
Yurick <yurick.hauschild@gmail.com>
2020-05-10 14:49:55 +0200

    ✨ Add closeRoomWhenLastOneLeaves function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,6 @@
 import * as admin from 'firebase-admin'
 export { updateVoteWhenJoiningRoom } from './update-vote-when-joining-room'
 export { updateVoteWhenLeavingRoom } from './update-vote-when-leaving-room'
+export { cleanRoomWhenLastOneLeaves } from './close-room-when-last-one-leaves'
 
 admin.initializeApp()
